fa55d19134f30d60210139d91f94b8de3e50fcd1
Andrew Nduati <andrew@paystack.com>
2023-12-05 13:43:22 +0300

    Initial Commit

---
+++ sample-kicks/sample_kicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_kicksApp.swift
+//  sample-kicks
+//
+//  Created by Andrew Nduati on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct sample_kicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
